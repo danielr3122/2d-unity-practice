e6ce838c25953e1f6bdbf452f3757d9a3ce0b42a
danielr3122 <drosenhamer@gmail.com>
2023-03-01 21:55:00 +0000

    Intial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    private float xMovementRange;
+    private float yMovementRange;
+    private float waterCount;
+    private float waterTime;
+    private float waterCollectTime;
+    private float waterLimit;
+    private bool nearWater;
+    private int seedCount;
+
+    [SerializeField] private float horizontalSpeed;
+    [SerializeField] private float verticalSpeed;
+
+    private GameObject waterSource;
+    private Camera mainCamera;
+    public GameObject seedPrefab;
+
+    private Rigidbody2D playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xMovementRange = 7.7f;
+        yMovementRange = 4.5f;
+        waterCount = 0;
+        waterTime = 0;
+        waterLimit = 3;
+        waterCollectTime = 1.5f;
+        seedCount = 0;
+        playerRb = GetComponent<Rigidbody2D>();
+        waterSource = GameObject.Find("Water Source");
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        waterTime += Time.deltaTime;
+        horizontalInput = Input.GetAxis("Vertical");
+        verticalInput = Input.GetAxis("Horizontal");
+        PlayerMovement();
+        ConstrainPlayer();
+        NearWater();
+        if(Input.GetMouseButtonDown(0)){
+            Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            // mousePosition.x -= Screen.width/2;
+            // mousePosition.y -= Screen.height/2;
+            Debug.Log("Mouse Down");
+            if(seedCount > 0){
+                Debug.Log("Shooting");
+                GameObject seed = Instantiate(seedPrefab, transform.position, transform.rotation);
+                seed.GetComponent<SeedController>().targetPosition = mousePosition;
+                Debug.Log("Mouse Position: " + mousePosition);
+                seed.GetComponent<SeedController>().startMoving = true;
+                seedCount--;
+            }
+        }
+    }
+
+    private void PlayerMovement(){
+        transform.Translate(Vector3.up * Time.deltaTime * horizontalInput * horizontalSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * verticalInput * verticalSpeed);
+    }
+
+    private void ConstrainPlayer(){
+        if(transform.position.x > xMovementRange){
+            transform.position = new Vector3(xMovementRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x < -xMovementRange){
+            transform.position = new Vector3(-xMovementRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.y > yMovementRange){
+            transform.position = new Vector3(transform.position.x, yMovementRange, transform.position.z);
+        }
+        if(transform.position.y < -yMovementRange){
+            transform.position = new Vector3(transform.position.x, -yMovementRange, transform.position.z);
+        }
+    }
+
+    private void NearWater(){
+        if((Mathf.Abs(waterSource.transform.position.x - transform.position.x) < 4) &&
+        (Mathf.Abs(waterSource.transform.position.y - transform.position.y) < 2)){
+            if(waterTime > waterCollectTime && waterCount < waterLimit){
+                waterCount++;
+                waterTime = 0;
+                Debug.Log("Water Count: " + waterCount);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Seed")){
+            seedCount++;
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SeedController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedController : MonoBehaviour
+{
+    public Vector3 targetPosition;
+    public bool startMoving;
+    private float xMovementRange;
+    private float yMovementRange;
+    private Vector3 targetDirection;
+    private bool isTargetCalculated;
+    
+    [SerializeField] private float seedSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xMovementRange = 10f;
+        yMovementRange = 7f;
+        seedSpeed = 5f;
+        isTargetCalculated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(startMoving){
+            if(!isTargetCalculated){
+                targetDirection = targetPosition - transform.position;
+                isTargetCalculated = true;
+            }
+            // transform.Translate((targetPosition - transform.position) * seedSpeed * Time.deltaTime);
+            transform.position += Time.deltaTime * targetDirection * seedSpeed;
+            // transform.position = Vector2.MoveTowards(transform.position, targetPosition, seedSpeed);
+        }
+        ConstrainSeed();
+    }
+
+    private void ConstrainSeed(){
+        if(transform.position.x > xMovementRange ||
+           transform.position.x < -xMovementRange ||
+           transform.position.y > yMovementRange ||
+           transform.position.y < -yMovementRange){
+            Destroy(gameObject);
+        }
+    }
+}
